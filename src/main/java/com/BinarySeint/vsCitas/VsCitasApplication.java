c7ef56982072a22b7fe04ad0275c862071535e3e
DoomedPlayer <ni.beeger@duocuc.cl>
2026-09-09 17:47:27 -0300

    Creación citas

---
+++ src/main/java/com/BinarySeint/vsCitas/VsCitasApplication.java
@@ -0,0 +1,13 @@
+package com.BinarySeint.vsCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsCitasApplication.class, args);
+	}
+
+}
